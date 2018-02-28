1f06f5130e1252ab7e713f40be826f34f7607020
dwwork <dwwork@gmail.com>
2018-02-28 13:19:51 -0500

    Fixed bug in test suite

--- test/func.C
+++ test/func.C
@@ -10,7 +10,7 @@ void quadratic_function(const AD::AutoDiff<2> &__restrict__ x, AD::AutoDiff<2> &
 void quadratic_function(const double &x, const double dx[2], double &f, double df[2]) {
        f = 3.0*x*x + 4.5*x + 1.0;
        for(int i=0;i<2;i++) {
-          df[i] = 6.0*x*dx[i] + 4.5;
+          df[i] = 6.0*x*dx[i] + 4.5*dx[i];
        }
 }
 
